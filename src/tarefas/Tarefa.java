68f55b8cd233ad0f844ed15e277db793e1f195ee
Victor H <victor.aquino@ccc.ufcg.edu.br>
2024-02-28 14:12:32 -0300

    criando classe tarefa

--- src/tarefas/Tarefa.java
+++ src/tarefas/Tarefa.java
@@ -1,5 +1,48 @@
 package tarefas;
 
 public class Tarefa {
+	private String titulo;
+	private String descricao;
+	private String vencimento;
+	private Integer prioridade;
+	
+	public Tarefa(String titulo, String descricao, String vencimento, Integer prioridade) {
+		this.setTitulo(titulo);
+		this.setDescricao(descricao);
+		this.setVencimento(vencimento);
+		this.setPrioridade(prioridade);
+	}
 
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getVencimento() {
+		return vencimento;
+	}
+
+	public void setVencimento(String vencimento) {
+		this.vencimento = vencimento;
+	}
+
+	public Integer getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+	
 }
