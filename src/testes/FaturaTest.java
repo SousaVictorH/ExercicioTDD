a1ffcd9b73cad30044e5ba559130bbce8433f950
felipesqra <felipe.siqueira@ccc.ufcg.edu.br>
2024-03-06 10:21:30 -0300

    Criando teste do método de criação e gets da classe Fatura

--- src/testes/FaturaTest.java
+++ src/testes/FaturaTest.java
@@ -4,7 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import faturas.Fatura;
+import tarefas.Fatura;
 
 public class FaturaTest {
+
+    @Test
+    void testCriaFatura() {
+        Fatura fatura = new Fatura("Nome", "Endereco", "TipoServico", 3);
+
+        assertEquals("Nome", fatura.getNome());
+        assertEquals("Endereco", fatura.getEndereco());
+        assertEquals("TipoServico", fatura.getServico());
+        assertEquals(3, fatura.getValor());
+    }
+
 }
